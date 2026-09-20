7cc01df5cf5a8351afbe91fcc699f427b50f06fe
Muneeb Ahmed <muneebrbutt1@outlookcom>
2026-09-20 19:53:11 +0500

    Added the multiple chats feature and new chat feature such that when the user would click on new chat, he would be able to chat in a converstaion and respective history would get saved

--- Chatbot_with_UI/langgraph_backend.py
+++ Chatbot_with_UI/langgraph_backend.py
@@ -1,6 +1,6 @@
 from langgraph.graph import StateGraph, START, END
 from typing import TypedDict, Annotated
-from langchain_core.messages import BaseMessage
+from langchain_core.messages import BaseMessage, HumanMessage
 from langchain_openai import ChatOpenAI
 from langgraph.checkpoint.memory import InMemorySaver
 from langgraph.graph.message import add_messages
@@ -34,7 +34,6 @@ graph.add_edge('chat_node', END)
 
 chatbot = graph.compile(checkpointer=checkpointer)
 
-        
 
 
 
--- Chatbot_with_UI/streamlit_frontend.py
+++ Chatbot_with_UI/streamlit_frontend.py
@@ -1,12 +1,64 @@
 import streamlit as st 
 from langgraph_backend import chatbot
 from langchain_core.messages import HumanMessage
+import uuid # Importing this to generate dynamic threadids rather than hardcoating it for each conversation.
 
 
-CONFIG= {'configurable': {'thread_id': 'chat-session-1'}}
+
+# Utility Functions
+def generate_thread_id():
+    thread_id = uuid.uuid4()
+    return thread_id
+
+# Function to start a new chat
+def reset_chat():
+    thread_id = generate_thread_id()
+    st.session_state['thread_id'] = thread_id
+    add_thread(st.session_state['thread_id']) 
+    st.session_state['message_history'] = []
+    
+def add_thread(thread_id):
+    if thread_id not in st.session_state['chat_threads']:
+        st.session_state['chat_threads'].append(thread_id)
+        
+# This function is loading conversations respectively.       
+def load_conversation(thread_id):
+    return chatbot.get_state(config={'configurable': {'thread_id': thread_id}}).values.get('messages', [])
+
+if 'thread_id' not in st.session_state:
+    st.session_state['thread_id'] = generate_thread_id()
+    
+if 'chat_threads' not in st.session_state:
+    st.session_state['chat_threads'] = []    
+    
+add_thread(st.session_state['thread_id'])    
+    
 if 'message_history' not in st.session_state:
     st.session_state['message_history'] = []
+    
+    
+    
+st.sidebar.title("Langgraph Chatbot")   
+if st.sidebar.button("New Chat"):
+    reset_chat() 
 
+st.sidebar.header("My Conversations")
+
+for thread_id in st.session_state['chat_threads']:
+    if st.sidebar.button(str(thread_id)):
+        st.session_state['thread_id'] = thread_id
+        messages = load_conversation(thread_id)
+        
+        temp_messages = []
+        for message in messages:
+            if isinstance(message, HumanMessage):
+                role = 'user'
+            else:
+                role = 'assistant'
+            temp_messages.append({'role':role, 'content':message.content})
+        st.session_state['message_history'] = temp_messages            
+
+    CONFIG = {'configurable': {'thread_id': st.session_state['thread_id']}}
 
 for message in st.session_state['message_history']:
     with st.chat_message(message['role']):
