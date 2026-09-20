23b8cc706e7765deaf7158bf13935e9e766d73c8
Muneeb Ahmed <muneebrbutt1@outlookcom>
2026-09-20 06:05:57 +0500

    Added streaming feature in the chatbot suchthat now the response would come in words streaming rathan coming all at once

--- Chatbot_with_UI/langgraph_backend.py
+++ Chatbot_with_UI/langgraph_backend.py
@@ -33,5 +33,8 @@ graph.add_edge(START, 'chat_node')
 graph.add_edge('chat_node', END)
 
 chatbot = graph.compile(checkpointer=checkpointer)
-chatbot    
+
+        
+
+
 
--- Chatbot_with_UI/streamlit_frontend.py
+++ Chatbot_with_UI/streamlit_frontend.py
@@ -2,17 +2,6 @@ import streamlit as st
 from langgraph_backend import chatbot
 from langchain_core.messages import HumanMessage
 
-# with st.chat_message('user'):
-#     st.text('Hello')
-    
-# with st.chat_message('assistant'):
-#     st.text('How can i assist u today')    
-
-# user_input = st.chat_input('Type Here')
-
-# if user_input:
-#     with st.chat_message('user'):
-#         st.text(user_input)
 
 CONFIG= {'configurable': {'thread_id': 'chat-session-1'}}
 if 'message_history' not in st.session_state:
@@ -32,8 +21,32 @@ if user_input:
         st.text(user_input)
      
     
-    response = chatbot.invoke({'messages': [HumanMessage(content=user_input)]}, config=CONFIG) 
-    ai_message = response['messages'][-1].content                 
-    st.session_state['message_history'].append({'role':'assistant', 'content': ai_message})    
+    # response = chatbot.invoke({'messages': [HumanMessage(content=user_input)]}, config=CONFIG) 
+    # ai_message = response['messages'][-1].content 
+                    
     with st.chat_message('assistant'):
-        st.text(ai_message)   
+        ai_message = st.write_stream(
+            message_chunk.content for message_chunk, metadata in chatbot.stream(
+                {'messages': [HumanMessage(content=user_input)]},
+                config=CONFIG,
+                stream_mode='messages'
+)
+        )
+        
+    st.session_state['message_history'].append({'role':'assistant', 'content': ai_message})       
+        
+
+
+
+# "Practicing streamlit code."
+# with st.chat_message('user'):
+#     st.text('Hello')
+    
+# with st.chat_message('assistant'):
+#     st.text('How can i assist u today')    
+
+# user_input = st.chat_input('Type Here')
+
+# if user_input:
+#     with st.chat_message('user'):
+#         st.text(user_input)          
